2fadf7f2e0035a18e5eb0f5f2e1ffb8c914aca53
Andreas Trepczik <andy@asrock.fritz.box>
2021-02-13 11:14:26 +0100

    container-structure-test: add more config options

--- container-structure-test/entrypoint.sh
+++ container-structure-test/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
 container-structure-test test \
-    --config config.yaml \
+    --config ${PLUGIN_CONFIG:-config.yaml} \
+    --driver ${PLUGIN_DRIVER:-docker}
+    --output ${PLUGIN_OUTPUT:-text}
     --image ${PLUGIN_IMAGE}
