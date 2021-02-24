f3525e6bd46dc69aad07c05436d86c50f540dc6e
Andreas Trepczik <andy@asrock.fritz.box>
2021-02-24 22:30:16 +0100

    container-structure-test: fix syntax

--- container-structure-test/entrypoint.sh
+++ container-structure-test/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 container-structure-test test \
-    --config ${PLUGIN_CONFIG:-config.yaml} \
-    --driver ${PLUGIN_DRIVER:-docker}
-    --output ${PLUGIN_OUTPUT:-text}
-    --image ${PLUGIN_IMAGE}
+    --config "${PLUGIN_CONFIG:-config.yaml}" \
+    --driver "${PLUGIN_DRIVER:-docker}" \
+    --output "${PLUGIN_OUTPUT:-text}" \
+    --image "${PLUGIN_IMAGE}"
